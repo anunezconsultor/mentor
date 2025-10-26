42f684678b906db730a08b2073ac797e426b3876
Adrián Núñez <adriannunez.consultor@gmail.com>
2025-10-26 14:37:17 +0100

    borrado form

--- assets/js/email.js
+++ assets/js/email.js
@@ -2,6 +2,24 @@ document.getElementById("contact-form").addEventListener("submit", async (e) => 
   e.preventDefault();
   const formData = Object.fromEntries(new FormData(e.target).entries());
 
+
+    var form = document.forms[0]; 
+
+    var nameElement = form.querySelector('input[name="name"]');
+    nameElement.value = "";
+
+    var emailElement = form.querySelector('input[name="email"]');
+    emailElement.value = "";
+
+    var subjectElement = form.querySelector('input[name="subject"]');
+    subjectElement.value = "";
+
+    var messageElement = form.querySelector('textarea[name="message"]');
+    messageElement.value = "";
+
+    var checkboxElement = form.querySelector('input[name="policy"]');
+    checkboxElement.checked = false;
+
   const res = await fetch("/.netlify/functions/send-email", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -12,6 +30,24 @@ document.getElementById("contact-form").addEventListener("submit", async (e) => 
 
   if (data.success) {
     document.querySelector(".sent-message").style.display = "block";
+
+    var form = document.forms[0]; 
+
+    var nameElement = form.querySelector('input[name="name"]');
+    nameElement.value = "";
+
+    var emailElement = form.querySelector('input[name="email"]');
+    emailElement.value = "";
+
+    var subjectElement = form.querySelector('input[name="subject"]');
+    subjectElement.value = "";
+
+    var messageElement = form.querySelector('textarea[name="message"]');
+    messageElement.value = "";
+
+    var checkboxElement = form.querySelector('input[name="policy"]');
+    checkboxElement.checked = false;
+
   } else {
     document.querySelector(".error-message").textContent = data.error || "Error al enviar el correo.";
   }
